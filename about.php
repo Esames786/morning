ece2568b387896c860ec2dd414af243f61dfa8fe
shariq <shariq.ali@bitswits.com>
2022-06-23 19:14:13 +0500

    changes

---
+++ about.php
@@ -0,0 +1,242 @@
+<!doctype html>
+<html lang="en">
+   <head>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+      <!-- favicon -->
+      <link rel="icon" type="image/png" href="assets/images/favicon.png">
+      <!-- Bootstrap CSS -->
+      <link rel="stylesheet" href="assets/vendors/bootstrap/css/bootstrap.min.css" media="all">
+      <!-- jquery-ui css -->
+      <link rel="stylesheet" type="text/css" href="assets/vendors/jquery-ui/jquery-ui.min.css">
+      <!-- fancybox box css -->
+      <link rel="stylesheet" type="text/css" href="assets/vendors/fancybox/dist/jquery.fancybox.min.css">
+      <!-- Fonts Awesome CSS -->
+      <link rel="stylesheet" type="text/css" href="assets/vendors/fontawesome/css/all.min.css">
+      <!-- Elmentkit Icon CSS -->
+      <link rel="stylesheet" type="text/css" href="assets/vendors/elementskit-icon-pack/assets/css/ekiticons.css">
+      <!-- slick slider css -->
+      <link rel="stylesheet" type="text/css" href="assets/vendors/slick/slick.css">
+      <link rel="stylesheet" type="text/css" href="assets/vendors/slick/slick-theme.css">
+      <!-- google fonts -->
+      <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400&family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,400&display=swap" rel="stylesheet">
+      <!-- Custom CSS -->
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <title>About | Traveler – Travel & Trip Business HTML5 Template</title>
+   </head>
+   <body>
+      <div id="siteLoader" class="site-loader">
+         <div class="preloader-content">
+            <img src="assets/images/loader1.gif" alt="">
+         </div>
+      </div>
+      <div id="page" class="page">
+         <!-- ***site header html start*** -->
+         <?php include "./includes/header.php"?>
+         <!-- ***site header html end*** -->
+         <main id="content" class="site-main">
+            <section class="inner-page-wrap">
+               <!-- ***Inner Banner html start form here*** -->
+               <div class="inner-banner-wrap">
+                  <div class="inner-baner-container" style="background-image: url(assets/images/img7.jpg);">
+                     <div class="container">
+                        <div class="inner-banner-content">
+                           <h1 class="page-title">About Us</h1>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <!-- ***Inner Banner html end here*** -->
+               <!-- ***about section html start form here*** -->
+               <div class="inner-about-wrap">
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-lg-8">
+                           <div class="about-content">
+                              <figure class="about-image">
+                                 <img src="assets/images/img27.jpg" alt="">
+                                 <div class="about-image-content">
+                                    <h3>WE ARE BEST FOR TOURS & TRAVEL SINCE 2004 !</h3>
+                                 </div>
+                              </figure>
+                              <h2>HOW WE ARE BEST FOR TRAVEL !</h2>
+                              <p>Dictumst voluptas qui placeat omnis repellendus, est assumenda dolores facilisis, nostra, inceptos. Ullam laudantium deserunt duis platea. Fermentum diam, perspiciatis cupidatat justo quam voluptate, feugiat, quaerat. Delectus aute scelerisque blanditiis venenatis aperiam rem. Tempore porttitor orci eligendi velit vel scelerisque minus scelerisque? Dis! Aenean! Deleniti esse aperiam adipiscing, sapiente? </p>
+                              <p>Ratione conubia incididunt nullam! Sodales, impedit, molestias consectetuer itaque magni ut neque, lobortis expedita corporis voluptatem natus praesent mollis quidem auctor curae, mattis laboris diamlorem iure nullam esse? Pariatur primis.</p>
+                           </div>
+                           <div class="client-slider white-bg">
+                              <figure class="client-item">
+                                 <img src="assets/images/client-img7.png" alt="">
+                              </figure>
+                              <figure class="client-item">
+                                 <img src="assets/images/client-img8.png" alt="">
+                              </figure>
+                              <figure class="client-item">
+                                 <img src="assets/images/client-img9.png" alt="">
+                              </figure>
+                              <figure class="client-item">
+                                 <img src="assets/images/client-img10.png" alt="">
+                              </figure>
+                              <figure class="client-item">
+                                 <img src="assets/images/client-img11.png" alt="">
+                              </figure>
+                           </div>
+                        </div>
+                        <div class="col-lg-4">
+                           <div class="icon-box">
+                              <div class="box-icon">
+                                 <i aria-hidden="true" class="fas fa-umbrella-beach"></i>
+                              </div>
+                              <div class="icon-box-content">
+                                 <h3>AFFORDABLE TOURS</h3>
+                                 <p>Iure doloremque saepe? Ultrices mi aliquam dis tempore incididunt sint, cumque dis temp!</p>
+                              </div>
+                           </div>
+                           <div class="icon-box">
+                              <div class="box-icon">
+                                 <i aria-hidden="true" class="fas fa-user-tag"></i>
+                              </div>
+                              <div class="icon-box-content">
+                                 <h3>BEST TOUR GUIDES</h3>
+                                 <p>Iure doloremque saepe? Ultrices mi aliquam dis tempore incididunt sint, cumque dis temp!</p>
+                              </div>
+                           </div>
+                           <div class="icon-box">
+                              <div class="box-icon">
+                                 <i aria-hidden="true" class="fas fa-headset"></i>
+                              </div>
+                              <div class="icon-box-content">
+                                 <h3>AFFORDABLE TOURS</h3>
+                                 <p>Iure doloremque saepe? Ultrices mi aliquam dis tempore incididunt sint, cumque dis temp!</p>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <!-- ***about section html start form here*** -->
+               <!-- ***callback section html start form here*** -->
+               <div class="bg-img-fullcallback" style="background-image: url(assets/images/img7.jpg);">
+                  <div class="overlay"></div>
+                  <div class="container">
+                     <div class="row">
+                        <div class="col-lg-8 offset-lg-2 text-center">
+                           <div class="callback-content">
+                              <div class="video-button">
+                                 <a  id="video-container" data-fancybox="video-gallery" href="https://www.youtube.com/watch?v=2OYar8OHEOU">
+                                    <i class="fas fa-play"></i>
+                                 </a>
+                              </div>
+                              <h2 class="section-title">ARE YOU READY TO TRAVEL? REMEMBER US !!</h2>
+                              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui, ut quis deserunt, fugit enim veniam sunt unde dolorem quos dignissimos rerum vel tempora tempore laudantium. Expedita animi dolorum inventore vitae.</p>
+                              <div class="callback-btn">
+                                 <a href="package.html" class="round-btn">View Packages</a>
+                                 <a href="about.html" class="outline-btn outline-btn-white">Learn More</a>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <!-- ***callback section html end here*** -->
+            </section>
+         </main>
+         <!-- ***site footer html start form here*** -->
+         <?php include "./includes/footer.php"?>
+         <!-- ***site footer html end*** -->
+         <a id="backTotop" href="#" class="to-top-icon">
+            <i class="fas fa-chevron-up"></i>
+         </a>
+         <!-- ***custom search field html*** -->
+         <div class="header-search-form">
+            <div class="container">
+               <div class="header-search-container">
+                  <form class="search-form" role="search" method="get" >
+                     <input type="text" name="s" placeholder="Enter your text...">
+                  </form>
+                  <a href="#" class="search-close">
+                     <i class="fas fa-times"></i>
+                  </a>
+               </div>
+            </div>
+         </div>
+         <!-- ***custom search field html*** -->
+         <!-- ***custom top bar offcanvas html*** -->
+         <div id="offCanvas" class="offcanvas-container">
+            <div class="offcanvas-inner">
+               <div class="offcanvas-sidebar">
+                  <aside class="widget author_widget">
+                     <h3 class="widget-title">OUR PROPRIETOR</h3>
+                     <div class="widget-content text-center"> 
+                        <div class="profile"> 
+                           <figure class="avatar"> 
+                              <img src="assets/images/img21.jpg" alt=""> 
+                           </figure> 
+                           <div class="text-content"> 
+                              <div class="name-title"> 
+                                 <h4> James Watson</h4> 
+                              </div> 
+                              <p>Accumsan? Aliquet nobis doloremque, aliqua? Inceptos voluptatem, duis tempore optio quae animi viverra distinctio cumque vivamus, earum congue, anim velit</p> 
+                           </div> 
+                           <div class="socialgroup"> 
+                              <ul> 
+                                 <li> <a target="_blank" href="#"> <i class="fab fa-facebook"></i> </a> </li> 
+                                 <li> <a target="_blank" href="#"> <i class="fab fa-google"></i> </a> </li> 
+                                 <li> <a target="_blank" href="#"> <i class="fab fa-twitter"></i> </a> </li> 
+                                 <li> <a target="_blank" href="#"> <i class="fab fa-instagram"></i> </a> </li> 
+                                 <li> <a target="_blank" href="#"> <i class="fab fa-pinterest"></i> </a> </li> 
+                              </ul> 
+                           </div> 
+                        </div> 
+                     </div>
+                  </aside>
+                  <aside class="widget widget_text text-center">
+                     <h3 class="widget-title">CONTACT US</h3>
+                     <div class="textwidget widget-text">
+                        <p>Feel free to contact and<br/> reach us !!</p>
+                        <ul>
+                           <li>
+                              <a href="tel:+01988256203">
+                                 <i aria-hidden="true" class="icon icon-phone1"></i>
+                                 +111 111 111
+                              </a>
+                           </li>
+                           <li>
+                              <a href="mailtop:info@domain.com">
+                                 <i aria-hidden="true" class="icon icon-envelope1"></i>
+                                 info@domain.com
+                              </a>
+                           </li>
+                           <li>
+                              <i aria-hidden="true" class="icon icon-map-marker1"></i>
+                              abc abc
+                           </li>
+                        </ul>
+                     </div>
+                  </aside>
+               </div>
+               <a href="#" class="offcanvas-close">
+                  <i class="fas fa-times"></i>
+               </a>
+            </div>
+            <div class="overlay"></div>
+         </div>
+         <!-- ***custom top bar offcanvas html*** -->
+      </div>
+
+      <!-- JavaScript -->
+      <script src="assets/vendors/jquery/jquery.js"></script>
+      <script src="assets/vendors/waypoint/waypoints.js"></script>
+      <script src="assets/vendors/bootstrap/js/bootstrap.min.js"></script>
+      <script src="assets/vendors/jquery-ui/jquery-ui.min.js"></script>
+      <script src="assets/vendors/countdown-date-loop-counter/loopcounter.js"></script>
+      <script src="assets/vendors/counterup/jquery.counterup.min.js"></script>
+      <script src="https://unpkg.com/imagesloaded@4/imagesloaded.pkgd.min.js"></script>
+      <script src="assets/vendors/masonry/masonry.pkgd.min.js"></script>
+      <script src="assets/vendors/slick/slick.min.js"></script>
+      <script src="assets/vendors/fancybox/dist/jquery.fancybox.min.js"></script>
+      <script src="assets/vendors/slick-nav/jquery.slicknav.js"></script>
+      <script src="assets/js/custom.min.js"></script>
+   </body>
+</html>
