c07966384945b2525b52ca08b64fda86fd8ba2d5
shariq <shariq.ali@bitswits.com>
2022-06-23 19:32:42 +0500

    changes

--- STUDY_IN_UK.php
+++ STUDY_IN_UK.php
@@ -35,7 +35,7 @@
          <main id="content" class="site-main">
             <!-- ***Inner Banner html start form here*** -->
             <div class="inner-banner-wrap">
-               <div class="inner-baner-container" style="background-image: url(assets/images/country/canada-place.jpg);">
+               <div class="inner-baner-container" style="background-image: url(assets/images/country/uk.jpg);">
                   <div class="container">
                      <div class="inner-banner-content">
                         <h1 class="page-title">Study in UK</h1>
